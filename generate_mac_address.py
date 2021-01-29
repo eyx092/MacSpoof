9c8e3438d1d16afab2c8d6a813e934146f462c59
eyx <72813338+eyx092@users.noreply.github.com>
2021-01-28 20:14:45 -0500

    Add files via upload

--- generate_mac_address.py
+++ generate_mac_address.py
@@ -1,8 +1,18 @@
-import random
-random_address = "%02x:%02x:%02x:%02x:%02x:%02x" % (random.randint(0, 255),
-                                   random.randint(0, 255),
-                                   random.randint(0, 255),
-                                   random.randint(0, 255),
-                                   random.randint(0, 255),
-                                   random.randint(0, 255))
+import random, sys
+
+if sys.platform == "win32":
+    random_address = ("%02x%02x%02x%02x%02x%02x" % (random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255))).upper()
+
+else:
+    random_address = "%02x:%02x:%02x:%02x:%02x:%02x" % (random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255),
+                                       random.randint(0, 255))
 print(random_address)
---
+++ windows_mac_spoof.ps1
@@ -0,0 +1,2 @@
+_reg add HKEY_LOCAL_MACHINESYSTEMCurrentControlSetControlClass{4D36E972-E325-11CE-BFC1-08002BE10318}_0004 /v NetworkAddress /d AABBCCDDEEFF FC /f
+Write-Output "Operation successful."
